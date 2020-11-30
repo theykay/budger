0d7a28c5c740b269b66067a763acf866743e2328
TheyKay <murraykyleb@gmail.com>
2020-11-29 20:26:13 -0700

    db file completed

--- public/db.js
+++ public/db.js
@@ -3,12 +3,28 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
+  // create boject store, set autoincrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+}
+
+request.onsuccess = function (event) {
   db = event.target.result;
 
   // check if app is online before reading from db
   if (navigator.online) {
     checkDatabase();
   }
+}
+
+request.onerror = function (event) {
+  console.log("does not compute", event.target.errorCode);
+}
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
 
 function checkDatabase() {
@@ -19,7 +35,7 @@ function checkDatabase() {
   // get all records from store and set to variable
   const getAll = store.getAll();
 
-  getAll.onsuccess = function() {
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
         method: "POST",
@@ -29,15 +45,15 @@ function checkDatabase() {
           "Content-Type": "application/json"
         }
       })
-      .then(response => response.json())
-      .then(() => {
-        // if success, open transaction on pending db
-        const transaction = db.transaction(["pending"], "readwrite");
-        // access pending object store
-        const store = transaction.objectStore("pending");
-        // clear all items in store
-        store.clear();
-      })
+        .then(response => response.json())
+        .then(() => {
+          // if success, open transaction on pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+          // access pending object store
+          const store = transaction.objectStore("pending");
+          // clear all items in store
+          store.clear();
+        })
     }
   }
 }
